f027f07c717806939ac3f6f08ed050c7a20d2e80
Irfan Asif <hashtag.irfan@gmail.com>
2022-06-18 22:18:44 +0530

    [NEW] 0auth2.0

--- FigmaApp.ts
+++ FigmaApp.ts
@@ -1,12 +1,118 @@
 import {
     IAppAccessors,
+    IAppInstallationContext,
+    IConfigurationExtend,
+    IEnvironmentRead,
+    IHttp,
     ILogger,
-} from '@rocket.chat/apps-engine/definition/accessors';
-import { App } from '@rocket.chat/apps-engine/definition/App';
-import { IAppInfo } from '@rocket.chat/apps-engine/definition/metadata';
+    IModify,
+    IPersistence,
+    IRead,
+} from "@rocket.chat/apps-engine/definition/accessors";
+import { App } from "@rocket.chat/apps-engine/definition/App";
+import { IAppInfo } from "@rocket.chat/apps-engine/definition/metadata";
+import {
+    IAuthData,
+    IOAuth2Client,
+    IOAuth2ClientOptions,
+} from "@rocket.chat/apps-engine/definition/oauth2/IOAuth2";
+import { IUser } from "@rocket.chat/apps-engine/definition/users";
+import { sendDM } from "./src/lib/messages";
+import { create as registerAuthorizedUser } from "./src/storage/users";
+import { createOAuth2Client } from "@rocket.chat/apps-engine/definition/oauth2/OAuth2";
+import { FigmaCommand } from "./command/FigmaCommand";
 
 export class FigmaApp extends App {
     constructor(info: IAppInfo, logger: ILogger, accessors: IAppAccessors) {
         super(info, logger, accessors);
+    }
+
+    public user: IUser;
+    public botName: string;
+
+    private oauth2ClientInstance: IOAuth2Client;
+    private oauth2Options: IOAuth2ClientOptions = {
+        alias: "figma",
+        accessTokenUri: "https://www.figma.com/api/oauth/token",
+        authUri: "https://www.figma.com/oauth",
+        refreshTokenUri: "https://www.figma.com/api/oauth/refresh",
+        revokeTokenUri: "https://api.figma.com/v1/oauth/revoke_token",
+        defaultScopes: ["file_read"],
+        authorizationCallback: this.authorizationCallback.bind(this),
+    };
+
+    private async authorizationCallback(
+        authData: IAuthData,
+        user: IUser,
+        read: IRead,
+        modify: IModify,
+        http: IHttp,
+        persistence: IPersistence
+    ) {
+        if (authData) {
+            await registerAuthorizedUser(read, persistence, user);
+        }
+
+        const text = `Authentication was successful! ✨
+        You will now be notified for all your Figma comments and notifications.
+        You can subscribe to your team channel with files you want to receive notifications from.
+        `;
+
+        await sendDM(read, modify, user, text, persistence);
+    }
+
+    public async onEnable(): Promise<boolean> {
+        this.user = (await this.getAccessors()
+            .reader.getUserReader()
+            .getByUsername(this.botName)) as IUser;
+
+        this.botName = "Figma.bot";
+        return true;
+    }
+
+    public async onInstall(
+        context: IAppInstallationContext,
+        read: IRead,
+        http: IHttp,
+        persistence: IPersistence,
+        modify: IModify
+    ): Promise<void> {
+        const user = context.user;
+        const welcomeMessage = `You’ve successfully installed Figma Rocket.Chat app! Now your Figma comments and notifications will show up here. :tada:
+        With Figma App, you can reply to file comments directly in a rocket chat channel. You will get notified when:
+        \xa0\xa0 • A new comment is added to a file you are collaborating on.
+        \xa0\xa0 • Someone replies to a comment you made.
+        \xa0\xa0 • You are tagged in a file.
+        \xa0\xa0 • You are invited to a file.
+        \xa0\xa0 • A file you are collaborating on is updated.
+
+        Some tips:
+        \xa0\xa0 • When you reply to a Figma comment here, your reply will automatically be added to the Figma file.
+        \xa0\xa0 • Type \` /figma connect \` to connect your figma account to the rocket.chat server.
+        \xa0\xa0 • Type \` /figma help  \` for command. `;
+        await sendDM(read, modify, user, welcomeMessage, persistence);
+    }
+
+    public getOauth2ClientInstance(): IOAuth2Client {
+        if (!this.oauth2ClientInstance) {
+            this.oauth2ClientInstance = createOAuth2Client(
+                this,
+                this.oauth2Options
+            );
+        }
+
+        return this.oauth2ClientInstance;
+    }
+
+    protected async extendConfiguration(
+        configuration: IConfigurationExtend,
+        environmentRead: IEnvironmentRead
+    ): Promise<void> {
+        await Promise.all([
+            this.getOauth2ClientInstance().setup(configuration),
+            configuration.slashCommands.provideSlashCommand(
+                new FigmaCommand(this)
+            ),
+        ]);
     }
 }
---
+++ src/lib/messages.ts
@@ -0,0 +1,130 @@
+import {
+    IModify,
+    IPersistence,
+    IRead,
+} from "@rocket.chat/apps-engine/definition/accessors";
+import { IRoom, RoomType } from "@rocket.chat/apps-engine/definition/rooms";
+import {
+    BlockBuilder,
+    IBlock,
+} from "@rocket.chat/apps-engine/definition/uikit";
+import { IUser } from "@rocket.chat/apps-engine/definition/users";
+import { NotificationsController } from "./notification";
+
+export async function getDirectRoom(
+    read: IRead,
+    modify: IModify,
+    appUser: IUser,
+    username: string
+): Promise<IRoom | undefined> {
+    const usernames = [appUser.username, username];
+
+    let room: IRoom;
+    try {
+        room = await read.getRoomReader().getDirectByUsernames(usernames);
+    } catch (error) {
+        throw new error("Could not get direct room");
+    }
+
+    if (room) {
+        return room;
+    } else {
+        let roomId: string;
+
+        const newRoom = modify
+            .getCreator()
+            .startRoom()
+            .setType(RoomType.DIRECT_MESSAGE)
+            .setCreator(appUser)
+            .setMembersToBeAddedByUsernames(usernames);
+        roomId = await modify.getCreator().finish(newRoom);
+        return await read.getRoomReader().getById(roomId);
+    }
+}
+
+export async function sendMessage(
+    modify: IModify,
+    room: IRoom,
+    sender: IUser,
+    message: string,
+    blocks?: BlockBuilder | [IBlock]
+): Promise<string> {
+    const msg = modify
+        .getCreator()
+        .startMessage()
+        .setSender(sender)
+        .setRoom(room)
+        .setGroupable(false)
+        .setParseUrls(false)
+        .setText(message);
+
+    if (blocks !== undefined) {
+        msg.setBlocks(blocks);
+    }
+
+    return await modify.getCreator().finish(msg);
+}
+
+export async function shouldSendMessage(
+    read: IRead,
+    persistence: IPersistence,
+    user: IUser
+): Promise<boolean> {
+    const notificationsController = new NotificationsController(
+        read,
+        persistence,
+        user
+    );
+    const notificationStatus =
+        await notificationsController.getNotificationsStatus();
+
+    return notificationStatus ? notificationStatus.status : true;
+}
+
+export async function sendNotification(
+    read: IRead,
+    modify: IModify,
+    user: IUser,
+    room: IRoom,
+    message: string,
+    blocks?: BlockBuilder
+): Promise<void> {
+    const appUser = (await read.getUserReader().getAppUser()) as IUser;
+
+    const msg = modify
+        .getCreator()
+        .startMessage()
+        .setSender(appUser)
+        .setRoom(room)
+        .setText(message);
+
+    if (blocks) {
+        msg.setBlocks(blocks);
+    }
+
+    return read.getNotifier().notifyUser(user, msg.getMessage());
+}
+
+export async function sendDM(
+    read: IRead,
+    modify: IModify,
+    user: IUser,
+    message: string,
+    persistence: IPersistence,
+    blocks?: BlockBuilder | [IBlock]
+): Promise<string> {
+    const appUser = (await read.getUserReader().getAppUser()) as IUser;
+    const targetRoom = (await getDirectRoom(
+        read,
+        modify,
+        appUser,
+        user.username
+    )) as IRoom;
+    const shouldSend = await shouldSendMessage(read, persistence, user);
+
+    if (!shouldSend) {
+        return "";
+    }
+
+    return await sendMessage(modify, targetRoom, appUser, message, blocks);
+}
